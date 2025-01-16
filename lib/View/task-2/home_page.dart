a4117dd05011330b09922efd1cfcd49ee01b48e4
Admin <hirenbambhaniya120@gmail.com>
2025-01-16 14:01:41 +0530

    16-1-25

--- lib/View/task-2/home_page.dart
+++ lib/View/task-2/home_page.dart
@@ -14,22 +14,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
   List<User> users = [];
   User? currentUser;
-  late AnimationController _animationController;
-  late Animation<double> _fadeInAnimation;
+  // late AnimationController _animationController;
+  // late Animation<double> _fadeInAnimation;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    _fadeInAnimation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
-    );
-
-    _animationController.forward();
     loadAllUsers();
   }
 
@@ -75,12 +65,6 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   }
 
   @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade900,
@@ -103,77 +87,74 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(16.0),
-          child: FadeTransition(
-            opacity: _fadeInAnimation,
-            child: users.isEmpty
-                ? const Center(child: CircularProgressIndicator())
-                : Column(
-              children: [
-                if (currentUser != null)
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundImage: NetworkImage(currentUser!.image),
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        "${currentUser!.firstName} ${currentUser!.lastName}",
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        currentUser!.email,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.white70,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                const Divider(color: Colors.white70),
-                const SizedBox(height: 16),
-                ...users.map((user) => Card(
-                  color: Colors.blueGrey.shade800,
-                  elevation: 2,
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ListTile(
-                    contentPadding: const EdgeInsets.all(12),
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(user.image),
+          child: users.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+            children: [
+              if (currentUser != null)
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(currentUser!.image),
                     ),
-                    title: Text(
-                      "${user.firstName} ${user.lastName}",
+                    const SizedBox(height: 16),
+                    Text(
+                      "${currentUser!.firstName} ${currentUser!.lastName}",
                       style: const TextStyle(
-                        fontSize: 18,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
-                    subtitle: Text(
-                      user.email,
-                      style: const TextStyle(color: Colors.white70),
+                    const SizedBox(height: 8),
+                    Text(
+                      currentUser!.email,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.white70,
+                      ),
                     ),
-                    trailing: const Icon(
-                      Icons.arrow_forward_ios,
+                    const SizedBox(height: 20),
+                  ],
+                ),
+              const Divider(color: Colors.white70),
+              const SizedBox(height: 16),
+              ...users.map((user) => Card(
+                color: Colors.blueGrey.shade800,
+                elevation: 2,
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(12),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(user.image),
+                  ),
+                  title: Text(
+                    "${user.firstName} ${user.lastName}",
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                       color: Colors.white,
                     ),
-                    onTap: () {
-
-                    },
                   ),
-                ))
-              ],
-            ),
+                  subtitle: Text(
+                    user.email,
+                    style: const TextStyle(color: Colors.white70),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+
+                  },
+                ),
+              ))
+            ],
           ),
         ),
       ),
